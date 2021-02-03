4e2358a6b03c0d051e15d6d08cf08a754b8fbf75
prior93 <Parashar.adhikary@gmail.com>
2021-02-03 13:38:01 +0200

    loginpage

--- LoginPage/ViewController.swift
+++ LoginPage/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UserTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInTextField: UIButton!
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        UserTextField.layer.cornerRadius = 22
+        passwordTextField.layer.cornerRadius = 22
+        signInTextField.layer.cornerRadius = 22
     }
 
 
